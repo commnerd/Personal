8d55a30b99efbc9cfb14d0ed5a6e0becb229105f
Michael Miller <commnerd@gmail.com>
2018-02-18 11:22:28 -0700

    Debugging added for resume page and tests added to ensure issue doesn't happen again.

---
+++ tests/Feature/PageControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Work\EmploymentRecord;
+use Tests\TestCase;
+
+class PageControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Home page tests
+     *
+     * @return void
+     */
+    public function testHomePage()
+    {
+        $response = $this->get(route('home'));
+
+        $response->assertSuccessful();
+
+        $response->assertSee('<a class="navbar-brand" href="'.route('home').'"><img height="100%" alt="Michael J. Miller" src="/storage/michael-j-miller-logo.png"></a>');
+        $response->assertSee('Family');
+        $response->assertSee('Quote');
+        $response->assertSee('Social');
+        $response->assertSee('Contact');
+        $response->assertDontSee('Resume');
+        $response->assertDontSee('Portfolio');
+    }
+
+    /**
+     * Home page tests with resume
+     *
+     * @return void
+     */
+    public function testHomePageWithResume()
+    {
+        EmploymentRecord::create([
+            'employer' => 'Test employer',
+            'location' => 'Test location',
+            'position' => 'Test position',
+            'start_date' => 'Aug 2015',
+            'end_date' => 'Dec 2015',
+            'bullets' => 'Test work experience',
+        ]);
+
+        $response = $this->get(route('home'));
+
+        $response->assertSuccessful();
+
+        $response->assertSee('<a class="navbar-brand" href="'.route('home').'"><img height="100%" alt="Michael J. Miller" src="/storage/michael-j-miller-logo.png"></a>');
+        $response->assertSee('Family');
+        $response->assertSee('Quote');
+        $response->assertSee('Social');
+        $response->assertSee('Contact');
+        $response->assertSee('Resume');
+        $response->assertDontSee('Portfolio');
+    }
+
+    /**
+     * Test basic homepage functionality
+     *
+     * @return void
+     */
+    public function testResumePage()
+    {
+        $response = $this->get(route('resume'));
+
+        $response->assertSuccessful();
+    }
+}
