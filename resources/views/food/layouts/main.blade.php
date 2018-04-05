00c95d42e21c398a2a74e14f32fe9c738bd32a9f
Michael Miller <commnerd@gmail.com>
2018-04-05 08:04:35 -0600

    Added viewport definition to head tag for better mobile display

--- resources/views/food/layouts/main.blade.php
+++ resources/views/food/layouts/main.blade.php
@@ -2,7 +2,13 @@
 <html>
     <head>
         <title>Food Order Organizer - @yield('title')</title>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <link rel="icon" href="/storage/michael-j-miller-logo.ico">
         <link rel="stylesheet" href="{{ elixir('/css/food/app.css') }}">
     </head>
     <body>
