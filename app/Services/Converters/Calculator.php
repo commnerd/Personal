c40e8a2747b4e1a3f00d0b304ed8ab56b8181617
Michael J. Miller <commnerd@gmail.com>
2020-04-20 12:41:59 -0600

    Added Terabyte range calculations to calculator

--- app/Services/Converters/Calculator.php
+++ app/Services/Converters/Calculator.php
@@ -4,26 +4,28 @@ namespace App\Services\Converters;
 
 class Calculator
 {
-	const METRIC_BASE = '';
-	const METRIC_GIGA = 'G';
-	const METRIC_MEGA = 'M';
-	const METRIC_KILO = 'K';
+    const METRIC_BASE = '';
+    const METRIC_GIGA = 'G';
+    const METRIC_MEGA = 'M';
+    const METRIC_KILO = 'K';
+    const METRIC_TERA = 'T';
 
-	const METRIC_SCALE = [
-		self::METRIC_BASE,
-		self::METRIC_KILO,
-		self::METRIC_MEGA,
-		self::METRIC_GIGA,
-	];
+    const METRIC_SCALE = [
+        self::METRIC_BASE,
+        self::METRIC_KILO,
+        self::METRIC_MEGA,
+        self::METRIC_GIGA,
+        self::METRIC_TERA,
+    ];
 
-	public static function metric($value, $precision = 0) {
-		$scale = 0;
+    public static function metric($value, $precision = 0) {
+        $scale = 0;
 
-		while($value >= 1000) {
-			$value = $value / 1000;
-			$scale++;
-		}
+        while($value >= 1000) {
+            $value = $value / 1000;
+            $scale++;
+        }
 
-		return number_format($value, $precision)." ".self::METRIC_SCALE[$scale];
-	}
+        return number_format($value, $precision)." ".self::METRIC_SCALE[$scale];
+    }
 }
