e1f822c50b70cb300b5a6373686a7d5b44efac2b
Michael J. Miller <commnerd@gmail.com>
2020-05-22 22:25:07 -0600

    Fixed blog management navigation in admin

--- app/Voyager/Widgets/BlogDimmer.php
+++ app/Voyager/Widgets/BlogDimmer.php
@@ -36,7 +36,7 @@ class BlogDimmer extends BaseDimmer
             'text'   => "You have {$count} {$lower} in your database. Click on button below to view {$lower}.",
             'button' => [
                 'text' => __("Manage Blog"),
-                'link' => route('blog.index'),
+                'link' => route('canvas'),
             ],
             'image' => 'storage/admin/blog.jpg',
         ]));
