d3149be2bad85f411c5e1ae26436fd472dff9e9c
Michael J. Miller <commnerd@gmail.com>
2021-01-28 08:36:46 -0700

    Added phone number and address infrastructure

---
+++ database/factories/Models/AddressPhoneFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Models;
+
+use App\Models\Models\AddressPhone;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AddressPhoneFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AddressPhone::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/Models/PhoneFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Models;
+
+use App\Models\Models\Phone;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PhoneFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Phone::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
