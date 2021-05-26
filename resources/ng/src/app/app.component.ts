9d0a7b828fb16c9053fcfcd35f897d30aa31c6de
Michael J. Miller <commnerd@gmail.com>
2021-05-25 21:26:58 -0600

    Added initial entrance page and fade effect

--- resources/ng/src/app/app.component.ts
+++ resources/ng/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from "@animations/fade.animation";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AppComponent {
   title = 'ng';
+
+  public getRouterOutletState(outlet: RouterOutlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
+  }
 }
--- resources/ng/src/app/app.module.ts
+++ resources/ng/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { CoverComponent } from '@pages/cover/cover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
