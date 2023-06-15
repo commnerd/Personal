696032460581f51ac1c7189ead76fea6ee54cbfe
Michael J. Miller <michael.miller@admetricspro.com>
2023-06-14 22:42:50 -0600

    Added bootstrap and public images for referencing in site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,3 +3,10 @@
 
 @section('title'){{ config('app.name') }}@hasSection('title') - @yield('title')@endif
 @endsection
+
+@section('content')
+<div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary">Primary button</button>
+</div>
+@endsection
