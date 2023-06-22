54708ea4098719e1f39435de55296fb466f0e356
Michael J. Miller <michael.miller@admetricspro.com>
2023-06-22 06:25:18 -0600

    Final touches to initial site update release

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.foundation')
+
+@section('content')
+<header>
+    <div id="comment">
+        <div>I'm currently doing maintenance on my site.  I'll have the site right back up.</div>
+    </div>
+</header>
+@endsection
