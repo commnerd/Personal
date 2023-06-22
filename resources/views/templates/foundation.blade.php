f7921146fa790c183355f1cd46402c04457fd1c2
Michael J. Miller <commnerd@gmail.com>
2023-06-21 22:46:50 -0600

    Fix for augmented page style and vite connection

--- resources/views/templates/foundation.blade.php
+++ resources/views/templates/foundation.blade.php
@@ -4,9 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="/storage/michael-j-miller-logo.ico">
-        <title>{{ config('app.name') }}@hasSection('title') - @yield('title')@endif
-@endsection</title>
-        @vite('resources/css/app.css')
+        <title>{{ config('app.name') }}
+            @hasSection('title')
+            @section('title') - @yield('title')
+            @endsection
+            @endif
+        </title>
+        @vite('resources/sass/app.scss')
     </head>
     <body class="antialiased">
         @yield('content')
