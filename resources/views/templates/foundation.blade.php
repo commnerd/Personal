206e39ffbcfbca162b7e0da2f8bac02c7ae8a150
Michael Miller <commnerd@gmail.com>
2023-07-02 14:49:19 -0600

    V2.1.0/git actions
    * Started adding github workflows
    
    * Incorporated /etc configs for deploy process
    
    * Updates to deployment process
    
    * Run actions against 2.1.x branch
    
    * Modified run tests to run on push

--- resources/views/templates/foundation.blade.php
+++ resources/views/templates/foundation.blade.php
@@ -3,13 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name') }}@hasSection('title') - @yield('title') @endif</title>
         <link rel="icon" href="/storage/michael-j-miller-logo.ico">
-        <title>{{ config('app.name') }}
-            @hasSection('title')
-            @section('title') - @yield('title')
-            @endsection
-            @endif
-        </title>
         @vite('resources/sass/app.scss')
     </head>
     <body class="antialiased">
