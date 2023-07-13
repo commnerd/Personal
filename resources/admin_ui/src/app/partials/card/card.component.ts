4d60018bb6a3ffd90445e416f7c6ddc704a7225a
Michael J. Miller <commnerd@gmail.com>
2023-07-12 21:22:08 -0600

    Cards starting to take form

--- resources/admin_ui/src/app/partials/card/card.component.ts
+++ resources/admin_ui/src/app/partials/card/card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input() background ?: string;
+  style ?: string;
+  
+  ngOnInit(): void {
+    if(this.background) {
+      this.style = `background-image: url(/storage/admin/${this.background})`;
+    }
+  }
 
 }
