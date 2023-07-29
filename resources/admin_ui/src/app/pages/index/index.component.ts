6f36b2a4e540781d693d488e54941db0ba2f4aaf
Michael J. Miller <michael.miller@admetricspro.com>
2023-07-29 14:41:18 -0500

    API pulling site stats

--- resources/admin_ui/src/app/pages/index/index.component.ts
+++ resources/admin_ui/src/app/pages/index/index.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+  stats$!: Observable<any>;
 
+  constructor(
+    private api: ApiService
+  ){}
+
+  ngOnInit(): void {
+    this.stats$ = this.api.get('/api/site_stats');
+  }
 }
