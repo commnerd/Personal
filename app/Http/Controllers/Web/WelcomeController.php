bba5173634224c16fb4171ddd5fe02e1a9f8fa2a
Michael J. Miller <commnerd@gmail.com>
2023-08-01 09:17:28 -0500

    Cache the quote of the day for the rest of the day

--- app/Http/Controllers/Web/WelcomeController.php
+++ app/Http/Controllers/Web/WelcomeController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Models\Quote;
 use Illuminate\Http\{Response,Request};
+use Illuminate\Support\Facades\Cache;
+use Carbon\Carbon;
 use Socialite;
 
 class WelcomeController extends Controller
 {
     public function index(): Response
     {
-        return response()->view('welcome', [ 'quote' => Quote::inRandomOrder()->first() ]);
+        $quote = Cache::get('qotd') ?? Quote::inRandomOrder()->first();
+        
+        if(is_null(Cache::get('qotd'))) {
+            Cache::put('qotd', $quote, now()->until('today 24:00'));
+        }
+
+        return response()->view('welcome', [ 'quote' => $quote ]);
     }
 }
