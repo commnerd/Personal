9843acffc89c25372ee0d0c5d8c53a451710f952
Michael J. Miller <commnerd@gmail.com>
2023-09-22 10:34:10 -0500

    Bind package to form

--- resources/admin_ui/src/app/pages/composer/edit/edit.component.ts
+++ resources/admin_ui/src/app/pages/composer/edit/edit.component.ts
@@ -11,7 +11,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class EditComponent implements OnInit {
   
-  @Input() package!: Package | number;
+  @Input() package!: Package;
 
   constructor(
     private packageService: PackageService,
