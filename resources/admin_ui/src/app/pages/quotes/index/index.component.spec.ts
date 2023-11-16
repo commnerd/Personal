6bdb342bb5e1a09a24d44556458a9fe2b0d603ef
Michael J. Miller <commnerd@gmail.com>
2023-11-15 23:22:24 -0600

    Update to admin_builder container and angular tests added

--- resources/admin_ui/src/app/pages/quotes/index/index.component.spec.ts
+++ resources/admin_ui/src/app/pages/quotes/index/index.component.spec.ts
@@ -2,15 +2,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IndexComponent } from './index.component';
 import { QuoteService } from '../../../services/models/quote.service';
-import { HttpClient, HttpHandler } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
+import { Observable, of } from 'rxjs';
+import { Quote } from '../../../interfaces/quote';
+import { TestDataPaginator } from '../../../../testing/TestDataPaginator';
+import { Paginated } from '../../../interfaces/laravel/paginated';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('IndexComponent', () => {
   let component: IndexComponent;
   let fixture: ComponentFixture<IndexComponent>;
   let quoteService: QuoteService;
-  let httpClient: HttpClient;
-  let httpHandler: HttpHandler;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,14 +21,48 @@ describe('IndexComponent', () => {
       declarations: [IndexComponent]
     });
     fixture = TestBed.createComponent(IndexComponent);
-    quoteService = TestBed.inject(QuoteService);
-    httpClient = TestBed.inject(HttpClient);
-    httpHandler = TestBed.inject(HttpHandler);
     component = fixture.componentInstance;
+    quoteService = TestBed.inject(QuoteService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should print "Something went very wrong." on bad server response', () => {
+    quoteService.list = () => of(null as unknown as Paginated<Quote>);
+    fixture = TestBed.createComponent(IndexComponent);
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('table').textContent).toEqual("Something went very wrong.");
+  });
+
+  it('should print "No quotes found." on empty paged response', () => {
+    quoteService.list = () => of((new TestDataPaginator([])).get());
+    fixture = TestBed.createComponent(IndexComponent);
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('table').textContent).toEqual("No quotes found.");
+  });
+
+  it('should print the quote information as passed in', () => {
+    let data = [{
+      id: 1,
+      quote: "A quote",
+      source: "Some Ref",
+      active: false,
+    }, {
+      id: 2,
+      quote: "Another quote",
+      source: "Another Ref",
+      active: true,
+    }];
+    quoteService.list = () => of((new TestDataPaginator(data)).get());
+    fixture = TestBed.createComponent(IndexComponent);
+    fixture.detectChanges();
+    let content = fixture.nativeElement.querySelector('table').textContent;
+    expect(content).toContain("A quote");
+    expect(content).toContain("Some Ref");
+    expect(content).toContain("Another quote");
+    expect(content).toContain("Another Ref");
   });
 });
