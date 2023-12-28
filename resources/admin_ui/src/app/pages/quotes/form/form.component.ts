eaa0759df23daf843ac79f7150cf299e524d9c85
Michael J. Miller <commnerd@gmail.com>
2023-12-28 09:57:41 -0600

    Introduced slide toggle active on quotes

--- resources/admin_ui/src/app/pages/quotes/form/form.component.ts
+++ resources/admin_ui/src/app/pages/quotes/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { Quote } from '@interfaces/quote';
 import { QuoteService } from '@services/models/quote.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, OnChanges {
 
   @Input() quote !: Quote | null;
   quoteForm!: FormGroup;
@@ -23,11 +23,21 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
     this.quoteForm = this.formBuilder.group({
       quote: '',
-      resource: '',
+      source: '',
       active: false,
     });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void
+  {
+    console.log(changes);
+
     if(this.quote) {
-      this.quoteForm.setValue(this.quote);
+      this.quoteForm.setValue({
+        quote: changes['quote']?.currentValue.quote,
+        source: changes['quote']?.currentValue.source,
+        active: changes['quote']?.currentValue.active
+      });
     }
   }
 
