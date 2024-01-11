72694c1eeb6b539655a183b426b25fa5fc1315e8
Michael J. Miller <commnerd@gmail.com>
2024-01-11 10:27:28 -0600

    A couple package management bug fixes

--- database/migrations/2020_10_26_031506_alter_composer_packages_table.php
+++ database/migrations/2020_10_26_031506_alter_composer_packages_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('composer_packages', function (Blueprint $table) {
-            $table->text('description');
+            $table->text('description')->default('');
         });
     }
 };
