71355627e73ed9d7a4e7d64e51d11cba4c4c07a8
Michael J. Miller <commnerd@gmail.com>
2024-02-25 00:49:03 -0600

    Final fixes for admin routes

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', [\App\Http\Controllers\Web\WelcomeController::class, 'index'])->name('web.home');
 
-Route::get('{admin}', function (string $admin) {
-    return view('admin');
+Route::get('{admin}', function (string $path) {
+    $contentType = 'text/html';
+
+    if($path == 'admin' || !file_exists(storage_path("app/$path"))) {
+        return response(file_get_contents(storage_path("app/admin/index.html")));
+    }
+
+    $extension = pathinfo(storage_path("app/$path"), PATHINFO_EXTENSION);
+
+    switch($extension) {
+    case 'js':
+        $contentType = 'application/javascript';
+        break;
+    case 'css':
+        $contentType = 'text/css';
+        break;
+    case 'json':
+    case 'map':
+        $contentType = 'application/json';
+        break;
+    case 'ico':
+        $contentType = 'image/vnd.microsoft.icon';
+        break;    
+    }
+    return response(file_get_contents(storage_path("app/$path")))->header('Content-Type', $contentType);
 })->where('admin', '^admin.*')->name('admin');
 
 Route::get('resume', [\App\Http\Controllers\Web\ResumeController::class, 'index'])->name('web.resume');
