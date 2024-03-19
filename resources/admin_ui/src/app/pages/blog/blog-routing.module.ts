f23f9aa731218e29dc11791ad64456eabc8d7477
Michael J. Miller <commnerd@gmail.com>
2024-03-19 08:53:50 -0500

    Edit route added to blog-routing module

--- resources/admin_ui/src/app/pages/blog/blog-routing.module.ts
+++ resources/admin_ui/src/app/pages/blog/blog-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from "@pages/blog/index/index.component";
 import {CreateComponent} from "@pages/blog/create/create.component";
+import {EditComponent} from "@pages/blog/edit/edit.component";
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
-  { path: 'posts/create', component: CreateComponent }
+  { path: 'posts/create', component: CreateComponent },
+  { path: 'posts/:id/edit', component: EditComponent },
 ];
 
 @NgModule({
