67fc699fa5238ab6caee765514b5f7c8bba75715
Michael J. Miller <commnerd@gmail.com>
2025-09-08 10:28:05 -0500

    Fixed session-based tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Livewire\LivewireServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Ensure session is started for CSRF token generation
+        $this->app['session']->start();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            LivewireServiceProvider::class,
+        ];
+    }
 }
