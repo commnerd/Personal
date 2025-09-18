4eb3d7de38afac8275d623b4317e6c18b003077c
Michael J. Miller <commnerd@gmail.com>
2025-09-17 22:19:27 -0500

    Icon now established

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Michael J. Miller | Digital Home</title>
+    <link rel="icon" href="/storage/michael-j-miller-logo.ico" sizes="any">
+    <link rel="apple-touch-icon" href="/storage/michael-j-miller-logo.ico">
     <script src="https://cdn.tailwindcss.com"></script>
   </head>
   <body class="bg-white text-gray-900 font-sans">
